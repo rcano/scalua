6d89cbe553006456412f8609d9be158bf4208db8
Rodrigo Cano <ioniviil@gmail.com>
2015-07-12 22:16:08 -0300

    implementing iterators

--- src/main/scala/scalua/LuaStdLib.scala
+++ src/main/scala/scalua/LuaStdLib.scala
@@ -5,13 +5,17 @@ import Predef.{ ??? }
 
 object LuaStdLib {
 
+  trait Iterator[R, State]
+
   def print(any: Any): Unit = Predef.println(any)
   def setmetatable(m: Map[_, _], m2: Map[_, _]) = ???
   def require(r: String): Any = ???
   def loadfile(f: String): () => Any = ???
   def loadstring(text: String): () => Any = ???
+  def ipairs[V](m: Map[Int, V]): Iterator[(Int, V), Unit] = ???
+  def pairs[K, V](m: Map[K, V]): Iterator[(K, V), Unit] = ???
 
-  trait Map[K, V] extends Dynamic {
+  trait Map[K, V] {
     def apply(k: K): Option[V]
     def update(k: K, v: V): Unit
     def size(): Int
@@ -25,7 +29,15 @@ object LuaStdLib {
     }
   }
 
-  def cfor(from: Int, to: Int, step: Int = 1)(f: Int => Unit) = ???
+  trait List[T] {
+    
+  }
+  object List {
+    def apply[T](elems: T*) = ???
+  }
+
+  def cfor(from: Int, to: Int, step: Int = 1)(f: Int => Unit): Unit = ???
+  def cfor[R, State](iterator: Iterator[R, State], state: State = null, init: State = null)(f: R => Unit): Unit = ???
 
   implicit class StringLib(val s: String) extends AnyVal {
     @extensionMethod
@@ -53,7 +65,7 @@ object LuaStdLib {
     def matches(pattern: String) = ???
 
     @extensionMethod
-    def format(args: Any*) = ???
+    def format(args: Any*): String = ???
 
     @extensionMethod
     def gmatch(pattern: String) = ???
